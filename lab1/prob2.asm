87fa51299fb26f17500fec246b25cd2737d908c4
ryanvuu <ryanvu077@gmail.com>
2026-04-07 23:27:00 -0700

    complete reverse program assembly

--- lab1/prob2.asm
+++ lab1/prob2.asm
@@ -1,7 +1,6 @@
 # declare global so programmer can see actual addresses.
 .globl welcome
 .globl promptOne
-.globl promptTwo
 
 #  Data Area (this area contains strings to be displayed during the program)
 .data
@@ -10,10 +9,10 @@ welcome:
 	.asciiz " This program reverses the binary of a positive integer \n\n"
 
 promptOne:
-	.asciiz " Enter integer 1: "
+	.asciiz " Enter a positive integer: "
 
-promptTwo: 
-	.asciiz " \n Enter integer 2: "
+newLine:
+	.asciiz "\n"
 
 #Text Area (i.e. instructions)
 .text
@@ -32,12 +31,35 @@ main:
   ori     $a0, 0x3b
 	syscall
 
-  
-
-
-  # Display promptTwo
-  ori     $v0, $0, 4
-
-	lui     $a0, 0x1001
-  ori     $a0, 0x50
+  # Read input into $t0
+	ori		  $v0, $0, 5
 	syscall
+	add		  $t0, $0, $v0
+
+  # $t1 is the counter variable
+  add     $t1, $0, $0
+
+  # Loop to construct reversed number
+  loop_reverse:
+    slti    $t2, $t1, 32    # if $t1 >= 32 set $t2 = 0
+    beq     $t2, $0, end
+
+    # construct reversed bit
+    andi    $t3, $t0, 1     # extract the leasst significant bit
+    sll     $t4, $t4, 1     # left shift result by one; $t4 holds result
+    or      $t4, $t4, $t3   # sets the extracted bit into the result
+    srl     $t0, $t0, 1     # right shift number by one
+
+    addi    $t1, $t1, 1     # increment counter
+    j       loop_reverse
+  
+  end:
+    # Print reversed number
+    ori		$v0, $0, 1
+    add		$a0, $0, $t4
+    syscall
+
+    # Exit
+    ori		$v0, $0, 10
+    syscall
+    
