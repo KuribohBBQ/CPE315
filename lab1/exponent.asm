11d976d35c32a39c10304f4339e71588f4ff5f33
ryanvuu <ryanvu077@gmail.com>
2026-04-10 19:08:27 -0700

    complete java exponent

--- lab1/exponent.asm
+++ lab1/exponent.asm
@@ -2,6 +2,34 @@
 # Section: 
 # Description: 
 
+# ---------- Java ----------
+# public class Exponent {
+#   public static int exponent(int x, int y) {
+
+#     if (y == 1) {
+#       return x;
+#     }
+
+#     int res = x;
+#     int current = 0;
+
+#     while (y > 1) {
+#       for (int i = x; i > 1; i--) {
+#         current += res;
+#       }
+#       res += current;
+#       current = 0;
+#       y--;
+#     }
+
+#     return res;
+#   }
+
+#   public static void main(String[] args) {
+#     System.out.println(exponent(15, 7));
+#   }
+# }
+# --------------------------
 
 # declare global so programmer can see actual addresses.
 .globl welcome
