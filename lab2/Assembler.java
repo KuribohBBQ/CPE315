c8ad436aef1dfedf72ac0b048cb2e9d0564b3ce0
ryanvuu <ryanvu077@gmail.com>
2026-04-17 12:12:56 -0700

    add logic to insert labels into a label hash map

--- lab2/Assembler.java
+++ lab2/Assembler.java
@@ -1,20 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class Assembler {
-    public LabelMap doFirstPass(List<String> lines) {
+    // public LabelMap doFirstPass(String fname, List<String> lines) {
+    public static void doFirstPass(String fname) {
+        int curAddr = 0;
         LabelMap labelMap = new LabelMap();
-    
-        return labelMap;
-    }
-
-    public static void main(String[] args) {
-        String fpath = "test2.asm";
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fpath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fname))) {
             String line;
 
             while ((line = reader.readLine()) != null) {
@@ -29,12 +25,41 @@ public class Assembler {
                     continue;
                 }
 
-                // Move to next line after printing
-                System.out.println(line);
-            }
+                // Label exists
+                // Two cases:
+                    // 1. Only label
+                    // 2. Label followed by instruction
+                if (line.indexOf(":") != -1) {
+                    String[] splitLine = line.split(":", 2);
+                    String label = splitLine[0].trim();
+                    splitLine[1] = splitLine[1].trim();
+                    // System.out.println(Arrays.toString(splitLine));
 
+                    labelMap.addLabel(label, curAddr);
+                    
+                    if (splitLine[1].isEmpty()) {
+                        // System.out.println(line);
+                        // System.out.println("Adding " + label + " at address " + curAddr);
+                        continue;
+                    }
+                }
+
+                // Instruction exists
+                // System.out.println(line);
+                // System.out.println("curAddr = " + curAddr);
+                curAddr += 4;
+            }
+            labelMap.printLabels();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // return labelMap;
+    }
+
+    public static void main(String[] args) {
+        String fname = "test2.asm";
+        // String fname = "test3.asm";
+        // String fname = "temp.asm";
+        doFirstPass(fname);
     }
 }
