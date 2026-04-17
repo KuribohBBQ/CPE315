ee0229efa06fe9bc262c6162c89ee1e8d9d56c16
ryanvuu <ryanvu077@gmail.com>
2026-04-16 22:59:40 -0700

    change variable to camel case

--- lab2/LabelMap.java
+++ lab2/LabelMap.java
@@ -2,19 +2,19 @@ import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class LabelMap {
-  HashMap<String, Integer> label_map = new HashMap<String, Integer>();
+  HashMap<String, Integer> labelMap = new HashMap<String, Integer>();
 
   // Add a label to the label mappings
   public void addLabel(String label, Integer addr) {
-    label_map.put(label, addr);
+    labelMap.put(label, addr);
   }
 
   // Get the address associated with a label
   public Integer getAddr(String label) {
     // Throw error if label doesn't exist
-    if (!label_map.containsKey(label)) {
+    if (!labelMap.containsKey(label)) {
       throw new NoSuchElementException("Label " + label + "doesn't exist");
     }
-    return label_map.get(label);
+    return labelMap.get(label);
   }
 }
