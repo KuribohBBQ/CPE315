923ec92e1e1868557fb12a17b20f2ff40c3e8093
ryanvuu <ryanvu077@gmail.com>
2026-04-17 12:22:45 -0700

    complete first pass

--- lab2/Assembler.java
+++ lab2/Assembler.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-
 public class Assembler {
-    // public LabelMap doFirstPass(String fname, List<String> lines) {
-    public static void doFirstPass(String fname) {
+    public static LabelMap doFirstPass(String fname) {
+    // public static void doFirstPass(String fname) {
         int curAddr = 0;
         LabelMap labelMap = new LabelMap();
         try (BufferedReader reader = new BufferedReader(new FileReader(fname))) {
@@ -33,33 +29,29 @@ public class Assembler {
                     String[] splitLine = line.split(":", 2);
                     String label = splitLine[0].trim();
                     splitLine[1] = splitLine[1].trim();
-                    // System.out.println(Arrays.toString(splitLine));
 
                     labelMap.addLabel(label, curAddr);
                     
                     if (splitLine[1].isEmpty()) {
-                        // System.out.println(line);
-                        // System.out.println("Adding " + label + " at address " + curAddr);
                         continue;
                     }
                 }
 
                 // Instruction exists
-                // System.out.println(line);
-                // System.out.println("curAddr = " + curAddr);
                 curAddr += 4;
             }
-            labelMap.printLabels();
+            // labelMap.printLabels();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // return labelMap;
+        return labelMap;
     }
 
     public static void main(String[] args) {
-        String fname = "test2.asm";
-        // String fname = "test3.asm";
+        // String fname = "test2.asm";
+        String fname = "test3.asm";
         // String fname = "temp.asm";
-        doFirstPass(fname);
+        LabelMap labelMap = doFirstPass(fname);
+        labelMap.printLabels();
     }
 }
