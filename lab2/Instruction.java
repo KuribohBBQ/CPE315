3c688add9cd2e9d4075e8aa022ecf10cd348ee3e
ryanvuu <ryanvu077@gmail.com>
2026-04-19 15:57:21 -0700

    add additional fields

--- lab2/Instruction.java
+++ lab2/Instruction.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Instruction
 {
     //constructor for instriction
@@ -6,12 +8,23 @@ public class Instruction
     //Opcode and funct will be in hex 0x
     private int opcode;
     private int funct;
+    
+    private String[] operands;
 
-    public Instruction(String name, int opcode, int funct)
+    // Label the instruction belongs to
+    private String label;
+
+    private int addr;
+
+
+    public Instruction(String name, int opcode, int funct, String[] operands, String label, int addr)
     {
         this.name = name;
         this.opcode = opcode;
         this.funct = funct;
+        this.operands = operands;
+        this.label = label;
+        this.addr = addr;
     }
 
     public String getName()
@@ -28,5 +41,30 @@ public class Instruction
     public int getFunct()
     {
         return funct;
+    }
+
+    public String[] getOperands()
+    {
+        return operands;
+    }
+
+    public String getLabel()
+    {
+        return label;
+    }
+
+    public int getAddr()
+    {
+        return addr;
+    }
+
+    public void printInstr() {
+        System.out.println("name: " + this.name);
+        System.out.println("opcode: " + this.opcode);
+        System.out.println("funct: " + this.funct);
+        System.out.println("operands: " + Arrays.toString(this.operands));
+        System.out.println("label: " + this.label);
+        System.out.println("addr: " + this.addr);
+        System.out.println();
     }
 }
