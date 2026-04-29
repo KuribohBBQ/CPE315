f4473af6a0a0d6648b8de33e81a3c8588b731be3
ryanvuu <ryanvu077@gmail.com>
2026-04-28 18:25:23 -0700

    parse help command

--- lab3/src/Emulator.java
+++ lab3/src/Emulator.java
@@ -11,13 +11,6 @@ class Emulator {
     this.instList = instList;
   }
 
-  void processCommand(String command, int pc) {
-    switch (command) {
-      default:
-        System.out.println("LOL");
-    }
-  }
-
   void executeInteractive() {
     Scanner scanner = new Scanner(System.in);
 
@@ -34,6 +27,47 @@ class Emulator {
 
     scanner.close();
 
+  }
+
+  void processCommand(String command, int pc) {
+    String[] cmdTokens = command.split("\\s+"); // split to account for s and m commands
+
+    // If want to be strict with ensuring all commands are given as seen in lab spec
+    // if (cmdTokens.length > 1 && 
+    //   !(cmdTokens[0].equalsIgnoreCase("s") ||
+    //     cmdTokens[0].equalsIgnoreCase("m"))) {
+    //   System.out.println("Bad command");
+    // }
+
+
+    switch (cmdTokens[0]) {
+      case "h":
+        displayHelp();
+        break;
+      case "d":
+        break;
+      case "s":
+        break;
+      case "r":
+        break;
+      case "m":
+        break;
+      case "c":
+        break;
+      default:
+        System.out.println("LOL");
+    }
+  }
+
+  void displayHelp() {
+    System.out.println("\nh = show help");
+    System.out.println("d = dump register state");
+    System.out.println("s = single step through the program (i.e. execute 1 instruction and stop)");
+    System.out.println("s num = step through num instructions of the program");
+    System.out.println("r = run until the program ends");
+    System.out.println("m num1 num2 = display data memory from location num1 to num2");
+    System.out.println("c = clear all registers, memory, and the program counter to 0");
+    System.out.println("q = exit the program\n");
   }
 
 
