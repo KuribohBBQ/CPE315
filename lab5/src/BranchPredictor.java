7fda9a0dfa98dedb61302ef1486955d17c7a957d
ryanvuu <ryanvu077@gmail.com>
2026-05-21 13:49:44 -0700

    fix print ghr and add ghr updating

--- lab5/src/BranchPredictor.java
+++ lab5/src/BranchPredictor.java
@@ -1,23 +1,30 @@
 public class BranchPredictor {
   private int ghr;
+  private int ghrSize;
   private int[] predictor;
   private int totalPreds = 0;
   private int correctPreds = 0;
 
   public BranchPredictor(int ghrSize) {
-    this.ghr = ghrSize;
+    this.ghr = 0;
+    this.ghrSize = ghrSize;
     predictor = new int[(int) Math.pow(2, ghrSize)];
   }
 
+  public void updateGHR(int actualPred) {
+    this.ghr = (this.ghr << 1) | actualPred;
+  }
+
   public void printGHR() {
-    for (int i = ghr - 1; i >= 0; i--) {
+    for (int i = ghrSize - 1; i >= 0; i--) {
       int bit = ghr & (1 << i);
+      bit = (bit >= 1) ? 1 : 0;
       System.out.printf("%-3d", bit);
     }
 
     System.out.println();
     
-    for (int i = ghr - 1; i >= 0; i--) {
+    for (int i = ghrSize - 1; i >= 0; i--) {
       System.out.printf("%-3d", i);
     }
 
@@ -51,6 +58,6 @@ public class BranchPredictor {
 
   public void printPredAcc() {
     double acc = calcPredAcc();
-    System.out.printf("accuracy %.2f%% (%d correct predictions, %d predictions)\n\n", acc * 100, this.correctPreds, this.totalPreds);
+    System.out.printf("\naccuracy %.2f%% (%d correct predictions, %d predictions)\n\n", acc * 100, this.correctPreds, this.totalPreds);
   }
 }
