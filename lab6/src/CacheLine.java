116554264664f242463841afd009219743245184
ryanvuu <ryanvu077@gmail.com>
2026-05-30 14:01:52 -0700

    add access time attribute to be used when finding lru

--- lab6/src/CacheLine.java
+++ lab6/src/CacheLine.java
@@ -3,10 +3,12 @@
 public class CacheLine {
   private int tag;
   private boolean isEmpty;
+  private long accessTime;
 
   public CacheLine() {
     this.tag = -1; // initialize to default value
     this.isEmpty = true;
+    this.accessTime = -1;
   }
 
   public int getTag() {
@@ -16,6 +18,14 @@ public class CacheLine {
   public void setTag(int tag) {
     this.tag = tag;
     this.isEmpty = false;
+  }
+
+  public long getAccessTime() {
+    return this.accessTime;
+  }
+
+  public void setAccessTime(long accessTime) {
+    this.accessTime = accessTime;
   }
 
   public boolean getIsEmpty() {
